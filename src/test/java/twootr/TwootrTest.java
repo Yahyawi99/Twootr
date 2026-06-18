834d1d01e84552265116bdd5c8b6aea2bb43267f
Yahyawi99 <yassinyahyawi26@gmail.com>
2026-06-18 21:11:29 +0100

    TDD and Push based communication

---
+++ src/test/java/twootr/TwootrTest.java
@@ -0,0 +1,12 @@
+package twootr;
+
+import org.junit.jupiter.api.Test;
+
+public class TwootrTest {
+  @Test
+  public void shouldBeAbleToAuthenticateUser() {
+    // receive logon message for valid user
+    // logon method returns new endpoint.
+    // assert that endpoint is valid
+  }
+}
